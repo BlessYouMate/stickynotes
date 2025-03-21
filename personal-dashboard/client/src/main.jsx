7c3a82ff2b0cbd325d7707594f937aac4e7ea244
Wiktor <wiktorjck@gmail.com>
2025-03-21 19:49:51 +0100

    fixing path to file in main.jsx

--- personal-dashboard/client/src/main.jsx
+++ personal-dashboard/client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 
-import NotesPage from './notes/NotesPage.jsx';
+import NotesPage from './notes/notesPage.jsx';
 
 // Context Provider
 import { TagsProvider } from "./notes/resources/TagsContext";
